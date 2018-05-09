0c150e6ade6755e850a2471c5885f98990a0c219
SebastianKapunkt <Kindt.Sebastian@outlook.com>
2018-05-09 13:39:05 +0200

    Fix save path for text

--- TypeInvader/Assets/Scripts/General/SaveLoad.cs
+++ TypeInvader/Assets/Scripts/General/SaveLoad.cs
@@ -1,29 +1,37 @@
 using UnityEngine;
 using System.Collections;
-using System.Runtime.Serialization.Formatters.Binary; 
+using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using System.Collections.Generic;
 
-public static class SaveLoad {
+public static class SaveLoad
+{
     public static Game game = new Game();
 
     //it's static so we can call it from anywhere
-    public static void Save(string[] keyWords) {
+    public static void Save(string[] keyWords)
+    {
         BinaryFormatter bf = new BinaryFormatter();
         Game.keyWords = keyWords;
-        FileStream file = File.Create (Application.persistentDataPath + "/savedGames.gd"); //you can call it anything you want
+        FileStream file = File.Create(Application.dataPath + "/savedGames.gd"); //you can call it anything you want
         bf.Serialize(file, game);
         file.Close();
-    }   
-     
-    public static string[] Load() {
-        if(File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
+    }
+
+    public static string[] Load()
+    {
+        if (File.Exists(Application.dataPath + "/savedGames.gd"))
+        {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            FileStream file = File.Open(Application.dataPath + "/savedGames.gd", FileMode.Open);
             game = (Game)bf.Deserialize(file);
             file.Close();
             return Game.keyWords;
         }
-        return new string[1]{"WHAAAT"};
+        else
+        {
+            Save(Game.keyWords);
+            return Game.keyWords;
+        }
     }
 }
